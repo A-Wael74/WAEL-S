dc662aa4767522ab92ed740c05caa48a5236d3e9
A-Wael74 <ahmedwael965@gmail.com>
2023-12-20 23:03:03 +0200

    CreateTask Added

---
+++ OS/test1/Objects/oskernel.d
@@ -0,0 +1,10 @@
+./objects/oskernel.o: osKernel.c osKernel.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h osCfg.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f446xx.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\CAIRO\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
---
+++ OS/test1/osCfg.h
@@ -0,0 +1,19 @@
+/************************************************************************/
+/*     Author: Ahmed Wael									         											*/
+/*     Date: 20 December 2023								       											*/
+/*     Version: V01											       	  											*/
+/************************************************************************/
+
+#ifndef OS_CFG_H
+#define OS_CFG_H
+
+
+#define NUM_OF_TASK 3
+
+#define TASK_STACKSIZE 100
+
+#define BUS_FREQ 16000000
+
+
+
+#endif
